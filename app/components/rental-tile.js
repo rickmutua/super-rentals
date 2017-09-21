c39c529610eb54935ebeeeebdb54203718b1ddbc
Erick <rick.mutua12@gmail.com>
2017-09-21 08:48:45 +0300

    made a few updates and created review model to hold reviews and route rental.

--- app/components/rental-tile.js
+++ app/components/rental-tile.js
@@ -3,7 +3,7 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   isImageShowing: false,
   updateRentalForm: false,
-  
+
   actions: {
     imageShow: function() {
       this.set('isImageShowing', true);
@@ -11,13 +11,6 @@ export default Ember.Component.extend({
       imageHide: function() {
         this.set('isImageShowing', false);
       },
-        update(rental, params) {
-          this.sendAction('update', rental, params);
-        },
-          delete(rental) {
-            if(confirm('Are you sure you want to delete this rental?')) {
-              this.sendAction('destroyRental', rental);
-            }
-    }
+
   }
 });
--- app/models/rental.js
+++ app/models/rental.js
@@ -6,6 +6,7 @@ export default DS.Model.extend({
   city: DS.attr(),
   type: DS.attr(),
   image: DS.attr(),
-  bedrooms: DS.attr()
+  bedrooms: DS.attr(),
+  reviews: DS.hasMany('review', {async: true})
 
 });
